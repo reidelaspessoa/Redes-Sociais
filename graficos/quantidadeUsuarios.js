9fd39f39059ea33a1c28baf238539a57da2f2692
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2024-11-19 09:10:58 -0300

    Add files via upload

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS } from "./common.js"
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -10,14 +11,32 @@ async function quantidadeUsuarios() {
         
         x: nomeDasRedes,
         y: quantidadeUsuarios,
-        type: 'bar'
-        
+        type: 'bar',
+        marker: {
+            color: getCSS('--primary-color')
+            
+            }
         }
         ]
+        const layout = {
+
+            plot_bgcolor: getCSS('--bg-color'),
+            paper_bgcolor: getCSS('--bg-color'),
+            title: {
+                text: 'Redes sociais com mais usuários no mundo',
+                x: 0,
+                font: {
+                  color: getCSS('--primary-color'),
+                  family: getCSS('--font'),
+                  size: 30
+                }
+                
+                }
+            }
         const grafico = document.createElement('div')
         grafico.className = 'grafico'
         
         document.getElementById('graficos-container').appendChild(grafico)
-        Plotly.newPlot(grafico, data)
+        Plotly.newPlot(grafico, data, layout)
     }
     quantidadeUsuarios()
